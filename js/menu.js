0b459a091ea10018edd2bb0b876207ae07c5d287
Paulo Henrique <107502102+Just1cup@users.noreply.github.com>
2024-01-04 16:54:59 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 const sideMenu = document.querySelector('.list');
 const menuIcons = document.querySelectorAll('.menu-icon');
-const closeIcon = document.getElementById('close'); // Assuming 'close' is the ID of the close icon
+const closeIcon = document.querySelectorAll('#close')
 
 function openMenu() {
     sideMenu.style.transition = '0.5s';
@@ -8,18 +8,26 @@ function openMenu() {
     sideMenu.style.background = '#ff004f';
     if (window.innerWidth < 600) {
         sideMenu.style.background = 'transparent';
-        closeIcon.style.background = 'transparent';
+        closeIcon.style.background = 'transparent'
     }
 }
 
 function closeMenu() {
     sideMenu.style.right = "-250px";
     sideMenu.style.background = 'transparent';
-    closeIcon.style.background = 'transparent';
+    closeIcon.style.backgroung = 'transparent';
 }
+
+
+function redirectToExternalLink() {
+    window.location.href = "https://just1cup.github.io/WeatherApp/";
+}
+
+document.getElementById("work1").addEventListener("click", redirectToExternalLink);
+
 
 // Event listener to open the menu
 menuIcons[1].addEventListener('click', openMenu); // Index 1 corresponds to the 'fa-bars' icon
 
 // Event listener to close the menu
-closeIcon.addEventListener('click', closeMenu);
+menuIcons[0].addEventListener('click', closeMenu); // Index 0 corresponds to the 'fa-xmark' icon
