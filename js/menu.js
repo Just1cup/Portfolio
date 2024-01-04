f1766aeb2de0471fff0f9eeffe94001c6c6b3483
Paulo Henrique <107502102+Just1cup@users.noreply.github.com>
2024-01-04 15:09:58 -0300

    Update menu.js
    Fix the menu appearing before oppening

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,14 @@
 const sideMenu = document.querySelector('.list')
 
 function openMenu() {
+    sideMenu.style.transition ='0.5s'
     sideMenu.style.right = "0";
-
+    sideMenu.style.background = '#ff004f'
+    if(device-innerWidth < '600px'){
+        sideMenu.style.background = 'transparent'
+    }
     
 }
 function closeMenu() {
-    sideMenu.style.right = "-200px";
+    sideMenu.style.right = "-250px";
 }
